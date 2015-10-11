8d8f00f870d04935f4a4ce62ce4ad60f54048505
Amanda Porto <amandaporto@msn.com>
2015-10-11 19:33:14 -0400

    Added omniauth for github

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  
   resources :definitions do
     collection do
       get :search
